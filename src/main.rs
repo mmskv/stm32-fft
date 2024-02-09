a5cce0dd3daadce7a5f22aeb5ce1c6b52ef7e832
mmskv <max.mishuk2014@gmail.com>
2024-02-09 18:47:16 +0300

    feat: phase shifted pwm with complementary pin

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ use defmt::*;
 use embassy_executor::Spawner;
 use embassy_stm32::gpio::OutputType;
 use embassy_stm32::time::hz;
-use embassy_stm32::timer::simple_pwm::{PwmPin, SimplePwm};
-use embassy_stm32::timer::Channel;
+use embassy_stm32::timer::complementary_pwm::{ComplementaryPwm, ComplementaryPwmPin};
+use embassy_stm32::timer::simple_pwm::PwmPin;
+use embassy_stm32::timer::*;
 use embassy_stm32::Config;
 use {defmt_rtt as _, panic_probe as _};
 
@@ -17,24 +18,30 @@ async fn main(_spawner: Spawner) {
 
     info!("Hello World!");
 
-    let ch3 = PwmPin::new_ch3(p.PB0, OutputType::PushPull);
-    let mut pwm = SimplePwm::new(
-        p.TIM3,
+    let ch1 = PwmPin::new_ch1(p.PC6, OutputType::PushPull);
+    let ch2 = ComplementaryPwmPin::new_ch2(p.PB14, OutputType::PushPull);
+
+    let mut pwm = ComplementaryPwm::new(
+        p.TIM8,
+        Some(ch1),
         None,
         None,
-        Some(ch3),
+        Some(ch2),
+        None,
+        None,
+        None,
         None,
         hz(1),
         Default::default(),
     );
     let max = pwm.get_max_duty();
-    pwm.enable(Channel::Ch3);
+
+    pwm.enable(Channel::Ch1);
+    pwm.enable(Channel::Ch2);
 
     info!("PWM initialized");
     info!("PWM max duty {}", max);
 
-    pwm.set_duty(Channel::Ch3, max / 2);
-
-    loop {
-    }
+    pwm.set_duty(Channel::Ch1, max / 2);
+    pwm.set_duty(Channel::Ch2, max / 2);
 }
