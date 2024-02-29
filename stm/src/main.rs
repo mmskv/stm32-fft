93c85936703d81f945da8a5b3fceb7bdebce657c
mmskv <max.mishuk2014@gmail.com>
2024-02-29 04:05:23 +0300

    fix: compilation fail in debug with big buffers

--- stm/src/main.rs
+++ stm/src/main.rs
@@ -62,6 +62,14 @@ fn main() -> ! {
 async fn async_main(spawner: Spawner) -> ! {
     info!("Hello world");
 
+    #[cfg(not(debug_assertions))]
+    fn example() {
+        info!("Enabling caches for release build");
+        let mut cp = cortex_m::Peripherals::take().unwrap();
+        cp.SCB.enable_icache();
+        cp.SCB.enable_dcache(&mut cp.CPUID);
+    }
+
     let mut config = Config::default();
     {
         use embassy_stm32::rcc::*;
@@ -95,9 +103,9 @@ async fn async_main(spawner: Spawner) -> ! {
 
     let mac_addr = [0x00, 0x00, 0xDE, 0xAD, 0xBE, 0xEF];
 
-    static PACKETS: StaticCell<PacketQueue<8, 8>> = StaticCell::new();
+    static PACKETS: StaticCell<PacketQueue<4, 4>> = StaticCell::new();
     let device = Ethernet::new(
-        PACKETS.init(PacketQueue::<8, 8>::new()),
+        PACKETS.init(PacketQueue::<4, 4>::new()),
         p.ETH,
         Irqs,
         p.PA1,
@@ -133,8 +141,10 @@ async fn async_main(spawner: Spawner) -> ! {
 
     info!("Network task initialized");
 
-    let mut tx_buffer = [0u8; 11680];
-    let mut deadbeef_buffer = [0u8; 11680];
+    const BUFSIZE: usize = 8760;
+
+    let mut tx_buffer = [0u8; BUFSIZE];
+    let mut deadbeef_buffer = [0u8; BUFSIZE];
     let mut rx_buffer = [0u8; 1460];
 
     // Fill the tx_buffer with the pattern 0xDEADBEEF
@@ -142,13 +152,12 @@ async fn async_main(spawner: Spawner) -> ! {
         chunk.copy_from_slice(&0xDEADBEEFu32.to_ne_bytes());
     }
 
-    // Total size to send is 10 MB
     let total_size_to_send = 10_000_000; // 10 MB in bytes
     let mut total_sent = 0usize;
 
     loop {
         let mut socket = TcpSocket::new(&stack, &mut rx_buffer, &mut tx_buffer);
-        socket.set_timeout(Some(embassy_time::Duration::from_secs(10)));
+        socket.set_timeout(Some(Duration::from_secs(10)));
 
         let remote_endpoint = (Ipv4Address::new(192, 168, 88, 69), 8000);
         info!("connecting...");
@@ -166,17 +175,15 @@ async fn async_main(spawner: Spawner) -> ! {
                 info!("write error: {:?}", e);
                 break;
             }
-            total_sent += 11680;
+            total_sent += BUFSIZE;
         }
 
         if total_sent >= total_size_to_send {
-            // Successfully sent 10 MB of data, you might want to close the socket or reset total_sent
             info!("Successfully sent 10 MB of data");
             socket.close();
         }
 
-        // Reset for a potential reconnection and retry
         total_sent = 0;
-        Timer::after_secs(1).await;
+        Timer::after_millis(1500).await;
     }
 }
